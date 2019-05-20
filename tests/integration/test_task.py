73780971a10e9b1ac93cfde6a0d6f28854600a42
jcubeta <jcubeta@algernon.solutions>
2019-05-20 12:57:52 -0400

    added listener arn for environmental variables

---
+++ tests/integration/test_task.py
@@ -0,0 +1,16 @@
+import os
+
+import pytest
+
+from toll_booth.tasks import handler
+
+
+@pytest.mark.credible_tasks_integration
+@pytest.mark.usefixtures('dev_s3_resource')
+class TestTask:
+    def test_task(self, test_task_event, mock_context):
+        os.environ['LEECH_LISTENER_ARN'] = 'arn:aws:sns:us-east-1:726075243133:leech-dev-Listener-1EV4D8VOW7L37'
+        os.environ['LISTENER_ARN'] = 'arn:aws:sns:us-east-1:726075243133:credible-dev-Listener-EI7SS094P7RY'
+        results = handler(test_task_event, mock_context)
+        assert results
+
--- tests/unit/test_task.py
+++ tests/unit/test_task.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from toll_booth.tasks import handler
@@ -7,6 +9,7 @@ from toll_booth.tasks import handler
 @pytest.mark.usefixtures('dev_s3_resource')
 class TestTask:
     def test_task(self, test_task_event, mock_context, mock_bullhorn):
+        os.environ['LEECH_LISTENER_ARN'] = 'mock_arn'
         results = handler(test_task_event, mock_context)
         assert results
         assert mock_bullhorn.called
